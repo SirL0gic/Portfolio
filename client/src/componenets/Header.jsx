6d85bee529143bf64c8bd008389eea4e08e7acb5
Abis <abisali@hotmail.com>
2023-06-16 22:32:58 +0400

    Update Header.jsx

--- client/src/componenets/Header.jsx
+++ client/src/componenets/Header.jsx
@@ -1,27 +1,28 @@
 import React, { useState } from "react";
 
 const DarkModeToggle = () => {
-    const [darkModeStatus, setDarkModeStatus] = useState(false);
-  
-    const handleToggle = () => {
-      setDarkModeStatus(!darkModeStatus);
-      console.log(darkModeStatus);
-    };
-  
-    return (
-      <label className={`toggle-switch ${darkModeStatus ? 'dark-theme' : 'light-theme'}`}>
-        <input type="checkbox" checked={darkModeStatus} onChange={handleToggle} />
-        <span className="slider round">
-        </span>
-      </label>
-    );
+  const [darkModeStatus, setDarkModeStatus] = useState(false);
+
+  const handleToggle = () => {
+    setDarkModeStatus(!darkModeStatus);
   };
+
+  return (
+    <label
+      className={`toggle-switch ${
+        darkModeStatus ? "dark-theme" : "light-theme"
+      }`}
+    >
+      <input type="checkbox" checked={darkModeStatus} onChange={handleToggle} />
+      <span className="slider round"></span>
+    </label>
+  );
+};
 
 let TopHeader = () => {
   return (
     <div className="flex-box-one">
-     
-     <DarkModeToggle/>
+      <DarkModeToggle />
     </div>
   );
 };

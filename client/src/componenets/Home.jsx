c7f2a5f0d5bed985c0002a54f9706de68031f149
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-06-19 17:02:46 +0400

    update

--- client/src/componenets/Home.jsx
+++ client/src/componenets/Home.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TopHeader from "./Header";
-import ProfileSection from './Profile';
+import ProfileSection from "./Profile";
+import TechStackSection from "./TechStack";
 
 let HomePage = () => {
   return (
     <Container fluid className="main-container">
-      <Row className='row-one'>
-        <Col className='col-one' lg={12}>
+      <Row className="row-one">
+        <Col className="col-one" lg={12}>
           <TopHeader />
         </Col>
       </Row>
-      <Row className='row-two'>
-        <Col className='col-two'  lg={12}>
-          <ProfileSection/>
+      <Row className="row-two">
+        <Col className="col-two" lg={12}>
+          <ProfileSection />
+        </Col>
+      </Row>
+      <Row className="row-four">
+        <Col className="col-five" lg={12}>
+          <TechStackSection />
         </Col>
       </Row>
     </Container>
--- client/src/componenets/TechStack.jsx
+++ client/src/componenets/TechStack.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 let TechStackSection = () => {
-    return null;
-
+  return (
+    <Container className="tech-stack-container">
+      <Row className="row-five">
+        <Col className="col-six" lg={12}>
+          <div className="tech-stack-headings">
+            <h1>My Tech Stack</h1>
+            <h2> Technologies I love working with ❤️</h2>
+          </div>
+        </Col>
+      </Row>
+      <Row className="row-six">
+        <Col className="col-seven"></Col>
+        <Col className="col-eight"></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default TechStackSection;

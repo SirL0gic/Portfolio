3eda17bc58528a212715d4933cfed1e43b12b065
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-06-19 12:25:28 +0400

    update

--- client/src/componenets/Profile.jsx
+++ client/src/componenets/Profile.jsx
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import profile from "../assets/pro.webp"
+import profile from "../assets/pro.webp";
 import hand from "../assets/hand.svg";
 
-let ProfileSection = () => {
+const TypingEffect = ({ text, typingDelay }) => {
+  const [displayedText, setDisplayedText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    let typingInterval;
+
+    if (currentIndex < text.length) {
+      typingInterval = setInterval(() => {
+        setDisplayedText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, typingDelay);
+    } else {
+      clearInterval(typingInterval);
+    }
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [text, currentIndex, typingDelay]);
+
+  return <span>{displayedText}</span>;
+};
+
+const ProfileSection = () => {
   return (
     <Container fluid className="profile-container">
       <Row className="row-three">
         <Col className="col-three" lg={6} s={12}>
           <div className="profile-text-area">
             <p>
-            Hi
-            <img
-              src={hand}
-              alt="hand"
-              className="hand-image"
-              style={{ width: "70px", height: "70px" }}
-            />
-              ,
+            Hi,
+              <img
+                src={hand}
+                alt="hand"
+                className="hand-image"
+                style={{
+                  width: "60px",
+                  height: "60px",
+                  marginBottom: "10px",
+                  marginLeft: "10px",
+                }}
+              />
             </p>
-            <p>I’m Abis</p>
-            <p>&amp; I build digital solutions</p>
+            <p>
+              <TypingEffect text="I’m Abis" typingDelay={50} />
+            </p>
+            <p>
+              <TypingEffect text="& I build digital solutions" typingDelay={100} />
+            </p>
           </div>
         </Col>
         <Col className="col-four" lg={6} s={12}>

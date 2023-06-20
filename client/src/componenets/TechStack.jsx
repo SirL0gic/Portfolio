8f3d7a082f72d4568c0c59eb6e9d8254d4192b03
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-06-20 12:19:52 +0400

    update

--- client/src/componenets/TechStack.jsx
+++ client/src/componenets/TechStack.jsx
@@ -4,12 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import htmlLogo from "../assets/html.svg";
 import cssLogo from "../assets/css.svg";
 import jsLogo from "../assets/js.svg";
+import nodeLogo from "../assets/node.svg";
+import pythonLogo from "../assets/python.svg";
+import gitLogo from "../assets/git.svg"
+import reactLogo from "../assets/react.svg"
+import awsLogo from "../assets/aws.svg"
+import figmaLogo from "../assets/figma.svg"
+import githubLogo from "../assets/hub.svg"
+import mongoLogo from "../assets/mongo.svg"
+import CodeLogo from "../assets/vscode.svg"
 
 let TechStackSection = () => {
   return (
     <Container className="tech-stack-container">
       <Row className="row-five">
-        <Col className="col-six" lg={12}>
+        <Col className="col-six" lg={12} sm={6}>
           <div className="tech-stack-headings">
             <h1>My Tech Stack</h1>
             <h2> Technologies I love working with ❤️</h2>
@@ -17,39 +26,70 @@ let TechStackSection = () => {
         </Col>
       </Row>
       <Row className="row-six">
-        <Col className="col-seven">
+        <Col className="col-seven" lg={6} sm={12}>
           <div className="tech-items-box-one">
             <ul className="tech-items-list-one">
               <li>
                 {" "}
-                <img src={htmlLogo} alt="htmp"/>
+                <img src={htmlLogo} alt="html" className="html-logo"/>
               </li>
               <li>
                 {" "}
-                <img src={cssLogo} alt="css"/>
+                <img src={cssLogo} alt="css" className="css-logo"/>
               </li>
               <li>
                 {" "}
-                <img src={jsLogo} alt="js"/>
+                <img src={jsLogo} alt="js" className="js-logo"/>
               </li>
             </ul>
             <ul className="tech-items-list-two">
               <li>
                 {" "}
-                <img src={htmlLogo} alt="htmp"/>
+                <img src={nodeLogo} alt="node" className="node-logo"/>
               </li>
               <li>
                 {" "}
-                <img src={cssLogo} alt="css"/>
+                <img src={pythonLogo} alt="python" className="python-logo"/>
               </li>
               <li>
                 {" "}
-                <img src={jsLogo} alt="js"/>
+                <img src={gitLogo} alt="git" className="git-logo"/>
+              </li>
+            </ul>
+          </div>
+        </Col >
+        <Col className="col-eight" lg={6} sm={12}>
+        <div className="tech-items-box-two">
+            <ul className="tech-items-list-one">
+              <li>
+                {" "}
+                <img src={reactLogo} alt="react"/>
+              </li>
+              <li>
+                {" "}
+                <img src={awsLogo} alt="aws"/>
+              </li>
+              <li>
+                {" "}
+                <img src={mongoLogo} alt="mongo"/>
+              </li>
+            </ul>
+            <ul className="tech-items-list-two">
+              <li>
+                {" "}
+                <img src={figmaLogo} alt="figma"/>
+              </li>
+              <li>
+                {" "}
+                <img src={CodeLogo} alt="vscode"/>
+              </li>
+              <li>
+                {" "}
+                <img src={githubLogo} alt="github"/>
               </li>
             </ul>
           </div>
         </Col>
-        <Col className="col-eight"></Col>
       </Row>
     </Container>
   );

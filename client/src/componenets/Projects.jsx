ba16cb081a2181f95ded2ba7f47c69117db6fdcd
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-06-23 11:49:12 +0400

    update

--- client/src/componenets/Projects.jsx
+++ client/src/componenets/Projects.jsx
@@ -9,44 +9,47 @@ import "aos/dist/aos.css";
 
 let project_list = [
   {
-  id: 1,  
-  image: "https://i.imgur.com/ApP42ec.jpg",
-  title: "ELEMNTS E-Com",
-  description: "An e-commerce website created for a sustainable fashion brand on Shopify.",
-  stack: "React - Node - HTML - CSS - JS - Liquid",
-  live: "elemnts.com",
-  code: ""},
+    id: 1,
+    image: "https://i.imgur.com/ApP42ec.jpg",
+    title: "ELEMNTS E-Com",
+    description:
+      "An e-commerce website created for a sustainable fashion brand on Shopify.",
+    stack: "Liquid - Node - HTML - CSS - JS",
+    live: "elemnts.com",
+    code: "",
+  },
 
   {
-    id: 2,  
+    id: 2,
     image: "",
     title: "Hire Hub",
-    description: "Open-source job application system that simplifies the job search process!",
+    description:
+      "Open-source job application system that simplifies the job search process!",
     stack: "React - Node/Express - MongoDB",
     live: "https://onedrive.live.com/?authkey=%21AKSxo4ZO4Lr7Lgc&id=D6B7302631DC2497%211355&cid=D6B7302631DC2497&parId=root&parQt=sharedby&o=OneUp",
-    code: "https://github.com/SirL0gic/HireHub"},
-
-]
+    code: "https://github.com/SirL0gic/HireHub",
+  },
+];
 
 let ProjectCard = (props) => {
   return (
     <Card className="project-card" style={{ width: "22rem" }}>
-      <Card.Img
-        className="project-card-image"
-        variant="top"
-        src={props.img}
-      />
+      <Card.Img className="project-card-image" variant="top" src={props.img} />
       <Card.Body>
         <Card.Title className="card-title">{props.title}</Card.Title>
-        <Card.Text className="card-dec">
-      {props.description}
+        <Card.Text className="card-dec">{props.description}</Card.Text>
+        <Card.Text className="card-teach-stack">
+          Tech Stack: {props.stack}
         </Card.Text>
-        <Card.Text className="card-teach-stack">Tech Stack:</Card.Text>
         <div className="card-links-flex-box">
           {" "}
           <img src={linkImage}></img>
-          <a className="card-link-one">Link Preview</a>
-          <a className="card-link-two">View Code</a>
+          <a className="card-link-one" href={props.live}>
+            Link Preview
+          </a>
+          <a className="card-link-two" href={props.code}>
+            View Code
+          </a>
         </div>
       </Card.Body>
     </Card>
@@ -71,7 +74,14 @@ let ProjectsSectionDesktop = () => {
       <div data-aos="flip-up" data-aos-duration="1000">
         <Row className="row-projects-list-one">
           <Col className="col-card-hold" lg={4}>
-            <ProjectCard img={project_list[0].image} title={project_list[0].title} />
+            <ProjectCard
+              img={project_list[0].image}
+              title={project_list[0].title}
+              description={project_list[0].description}
+              stack={project_list[0].stack}
+              live={project_list[0].live}
+              code={project_list[0].code}
+            />
           </Col>
           <Col className="col-card-hold" lg={4}>
             {" "}
@@ -99,7 +109,9 @@ let ProjectsSectionDesktop = () => {
             <ProjectCard />
           </Col>
           <br />
-          <a className="see-all-proj" href="https://github.com/SirL0gic">See All Projects</a>
+          <a className="see-all-proj" href="https://github.com/SirL0gic">
+            See All Projects
+          </a>
         </Row>
       </div>
     </Container>
